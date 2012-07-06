484607a367b6610cb4712219faa992129ddbecd4
BoomBaster <sunrise.alex.mail@gmail.com>
2012-07-06 11:29:44 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = '22afe66b0c275a87985c0b7b2347c4ea044e154d556d5af263dddb575459932076fc761a7ff309a6f9ba1fdedc1ae3d95a54f4cac2ee4839e1f7c42dbf6e52a2'
